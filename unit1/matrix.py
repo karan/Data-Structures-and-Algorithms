f1011b21b4b5584a7535f6f66f87bf3a821fde44
Karan Goel <karan@goel.im>
2013-08-22 21:16:40 +0530

    matrix

---
+++ unit1/matrix.py
@@ -0,0 +1,21 @@
+# Matrix
+# @author: Karan Goel
+
+class Matrix(object):
+
+    def __init__(self, numberOfRows, numberOfColumns):
+        assert numberOfRows >= 0
+        assert numberOfColumns >= 0
+        super(Matrix, self).__init__()
+        self._numberOfRows = numberOfRows
+        self._numberOfColumns = numberOfColumns
+
+    def getNumberOfRows(self):
+        return self._numberOfRows
+
+    numberOfRows = property(fget=lambda self: self.getNumberOfRows())
+
+    def getNumberOfColumns(self):
+        return self._numberOfColumns
+
+    numberOfColumns = property(fget=lambda self: self.getNumberOfColumns())
